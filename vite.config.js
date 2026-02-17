abb36ef571b70aecfc031efc0de63e27766d887a
royverse1 <royverse1@gmail.com>
2026-02-17 14:02:52 +0530

    Deployment fix: removed absolute path from script source

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/YCS-New/', // This ensures assets load from the correct subfolder
+  base: '/YCS-New/', // Ensures assets load from the correct subfolder
 })
